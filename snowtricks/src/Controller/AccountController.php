c041a562cdcbc86fb4d86b71193d38e880fee149
Julien_Plumecocq <b4ckfr0mh3ll@gmail.com>
2020-06-08 17:10:48 +0200

    make account controller and form for registration and connection. Add default avatar if none uploaded, navbar updated

---
+++ snowtricks/src/Controller/AccountController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Service\FileUploader;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AccountController extends AbstractController
+{
+    /**
+     * Manage user authentification
+     * @Route("/login", name="account_login")
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }
+
+    /**
+     * manage user logout
+     * @Route("/logout", name="account_logout")
+     */
+    public function logout()
+    {
+
+    }
+
+    /**
+     * Show registration form and handle it
+     * @Route("/register", name="account_register")
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordEncoderInterface $encoder
+     * @param FileUploader $fileUploader
+     * @return Response
+     */
+
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder, FileUploader $fileUploader)
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $avatar = $form['avatar']->getData();
+            if ($avatar) {
+                $avatarFileName = $fileUploader->upload($avatar);
+                $user->setAvatar($avatarFileName);
+            }
+
+            $hash = $encoder->encodePassword($user, $user->getHash());
+            $user->setHash($hash);
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été créé !"
+            );
+
+            return $this->redirectToRoute('account_login');
+        }
+
+        return $this->render('account/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
